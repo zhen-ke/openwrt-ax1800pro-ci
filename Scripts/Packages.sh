be79440a67ae41ff75381012e630f7c6de1f25c7
XMit <xmit0806@outlook.com>
2025-10-03 21:38:59 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -71,7 +71,6 @@ UPDATE_PACKAGE "qmodem" "FUjr/QModem" "main"
 UPDATE_PACKAGE "quickfile" "sbwml/luci-app-quickfile" "main"
 UPDATE_PACKAGE "viking" "VIKINGYFY/packages" "main" "" "luci-app-timewol luci-app-wolplus"
 UPDATE_PACKAGE "vnt" "lmq8267/luci-app-vnt" "main"
-UPDATE_PACKAGE "wrtbwmon" "brvphoenix/luci-app-wrtbwmon" "master"
 
 #更新软件包版本
 UPDATE_VERSION() {
