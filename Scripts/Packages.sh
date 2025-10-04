7cc2de85f48a89a2e03ae5c7684197dacdc4f2d5
XMit <xmit0806@outlook.com>
2025-10-04 18:41:29 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -71,6 +71,7 @@ UPDATE_PACKAGE "qmodem" "FUjr/QModem" "main"
 UPDATE_PACKAGE "quickfile" "sbwml/luci-app-quickfile" "main"
 UPDATE_PACKAGE "viking" "VIKINGYFY/packages" "main" "" "luci-app-timewol luci-app-wolplus"
 UPDATE_PACKAGE "vnt" "lmq8267/luci-app-vnt" "main"
+UPDATE_PACKAGE "diskman" "lisaac/luci-app-diskman" "master"
 
 #更新软件包版本
 UPDATE_VERSION() {
