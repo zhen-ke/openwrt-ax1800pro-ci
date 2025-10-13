3148c7d223cecbb8ad7d346f1a126a3c2003050e
XMit <xmit0806@outlook.com>
2025-10-14 06:42:27 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -73,7 +73,6 @@ UPDATE_PACKAGE "viking" "VIKINGYFY/packages" "main" "" "luci-app-timewol luci-ap
 UPDATE_PACKAGE "vnt" "lmq8267/luci-app-vnt" "main"
 UPDATE_PACKAGE "diskman" "lisaac/luci-app-diskman" "master"
 UPDATE_PACKAGE "diskman" "lisaac/luci-app-diskman" "master"
-UPDATE_PACKAGE "momo" "nikkinikki-org/OpenWrt-momo" "main"
 
 #更新软件包版本
 UPDATE_VERSION() {
